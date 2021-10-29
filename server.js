4734431ab5b1db74aa1a0ad2050655238f8dae60
unknown <0225806@up.edu.mx>
2021-10-28 21:49:41 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// Create server
+const express = require('express')
+const mongoose = require('mongoose') // Database
+const Article = require('./DB_models/article') // Database model
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+//Importar variables de entorno
+require('dotenv').config({path: 'variables.env'})
+//console.log(process.env.DB_URL);
+
+mongoose.connect(process.env.DB_URL) // Connect to DB, no warnings
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
+
+app.use(express.static("public"));
+
+// Route for localhost
+app.get('/', async (req, res) =>{
+    const articles = await Article.find().sort({date: 'desc'})
+    res.render('articles/index', {articles: articles})  // View
+})
+app.use('/articles', articleRouter)
+
+//Variables y puerto de LocalHost
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+// Port
+app.listen(port, host, () => {
+    console.log('The server is running correctly');
+});
