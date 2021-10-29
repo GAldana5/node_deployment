8ff6279420c51cf7dd1628f173e11f323fe36bb3
unknown <0225806@up.edu.mx>
2021-10-29 00:27:25 -0500

    b

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Article = require('./DB_models/article') // Database model
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
+const timeout = require('connect-timeout')
 app.use(timeout('5s'))
 
 //Importar variables de entorno
